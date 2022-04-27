0e7f79b94f9f594b01a42d0e553b296832f369d6
Ibrahim Mattus Neto <2496339+IMATTUS@users.noreply.github.com>
2022-04-27 06:48:02 -0300

    Creating a simple screen to sum values

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,17 +1,43 @@
 package gui;
 
+import java.util.Locale;
+
 import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 
 public class ViewController {
-	@FXML
-	private Button btTest;
 	
 	@FXML
-	public void onBtTestAction() {
-		System.out.println("Click ");
-		Alerts.showAlert("Alert Title", "Alert Header", "Hello", AlertType.INFORMATION);
+	private TextField txtNumber1;
+	
+	@FXML
+	private TextField txtNumber2;
+	
+	@FXML
+	private Label labelResult;
+	
+	@FXML
+	private Button btSum;
+	
+	@FXML
+	public void onBtSumAction() {
+		try {
+			Locale.setDefault(Locale.US);
+	//		System.out.println("Click ");
+	//		Alerts.showAlert("Alert Title", "Alert Header", "Hello", AlertType.INFORMATION);
+			double number1 = Double.parseDouble(txtNumber1.getText());
+			double number2 = Double.parseDouble(txtNumber2.getText());
+			
+			double sum = number1 + number2;
+			
+			labelResult.setText(String.format("%.2f", sum));
+		}catch (NumberFormatException e) {
+			Alerts.showAlert("Error", "Parse error", e.getMessage(), AlertType.ERROR);
+			
+		}
 	}
 }
